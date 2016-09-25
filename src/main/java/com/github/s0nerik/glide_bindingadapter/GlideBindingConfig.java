5227a11e643b9840c0c3f244e44d181405537913
Alex Isaienko <thesonerik@gmail.com>
2016-09-25 03:15:08 +0300

    Version 1

---
+++ src/main/java/com/github/s0nerik/glide_bindingadapter/GlideBindingConfig.java
@@ -0,0 +1,35 @@
+package com.github.s0nerik.glide_bindingadapter;
+
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.DrawableRequestBuilder;
+import com.bumptech.glide.DrawableTypeRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GlideBindingConfig {
+    public interface Provider {
+        @NonNull
+        DrawableRequestBuilder<?> provide(DrawableTypeRequest<?> request);
+    }
+
+    private static Map<Object, Provider> providers = new HashMap<>();
+    private static Provider defaultProvider = null;
+
+    static Provider getProvider(Object key) {
+        return providers.get(key);
+    }
+
+    static Provider getDefaultProvider() {
+        return defaultProvider;
+    }
+
+    public static void registerProvider(Object key, Provider provider) {
+        providers.put(key, provider);
+    }
+
+    public static void setDefault(Object key) {
+        defaultProvider = providers.get(key);
+    }
+}
